5c796ee8a43ea0282d56ceec076ebd0e2e079a21
bond007alex <bond007alex@gmail.com>
2023-11-28 10:18:23 -0800

    Fixed enemy AI
    Broke enemy AI by adding collision and other cool things, but I built a new AI here, and it works for now. Will want more functionality later

--- enmv.gd
+++ enmv.gd
@@ -1,8 +1,13 @@
-extends Node2D
-var player = null
+extends CharacterBody2D
 var speed = 25
-func _ready():
-	player = $"player"
+# Movement speed
+var player_position
+var target_position
+#look for player reference
+@onready var player = get_tree().get_nodes_in_group("player")[0]
 func _physics_process(delta):
-		position += (player.position - position)/speed
-	
+	#find player
+	player_position = player.position
+	#move to player and orient towards player
+	velocity = position.direction_to(player_position)*speed
+	move_and_slide()
